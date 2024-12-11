965bad7512eafc71ea1afff24c33b1774450f15d
Aeroel <hmm>
2024-12-11 13:22:59 +0300

    Seems like the new lines behaviour when compared in the textcomparer is odd. For example, if I only add a newline each time I call appendXML, I can do \r\n or \n and it works. But if I do \r\n\r\n it fails. Aren't they just many newlines? This behaviour overall seems rather mysterious to me, but whatever, I guess

--- Helper_Functions.js
+++ Helper_Functions.js
@@ -1,7 +1,10 @@
 export { Helper_Functions };
 import { Config } from "./Config.js";
 class Helper_Functions {
-    static newlines = {linux: '\n', "windows": '\r\n'}
+    static newlines = {
+        linux: '\n',
+         "windows": '\r\n'
+        }
     static getNewline(desiredType = false) {
         if(desiredType !== false) {
             return this.newlines[desiredType];
